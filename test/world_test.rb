c3db2de12e93632563c48698d54c6570e595fb3e
Carl Mercier <carl@carlmercier.com>
2012-01-17 01:59:06 -0500

    require test_helper based on the full expanded path

--- test/world_test.rb
+++ test/world_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__)+'/test_helper'
+require File.expand_path(File.dirname(__FILE__))+'/test_helper'
 
 # Test Socialization as it would be used in a "real world" scenario
 class WorldTest < Test::Unit::TestCase
