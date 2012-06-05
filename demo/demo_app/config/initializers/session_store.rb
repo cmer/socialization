7e81a4d2d3e07224ba26248f00bfbfd33e034ccc
Carl Mercier <carl@carlmercier.com>
2012-06-05 11:24:55 -0400

    Made demo app 1.8.7 compliant

--- demo/demo_app/config/initializers/session_store.rb
+++ demo/demo_app/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DemoApp::Application.config.session_store :cookie_store, key: '_demo_app_session'
+DemoApp::Application.config.session_store :cookie_store, :key => '_demo_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
