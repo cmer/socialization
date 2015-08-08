4651d687faf20b05b3a1984bca96c30ea7a4c20f
Carl Mercier <carl@carlmercier.com>
2015-08-07 23:19:56 -0400

    RSpec AR store

--- spec/spec_support/matchers.rb
+++ spec/spec_support/matchers.rb
@@ -16,3 +16,39 @@ RSpec::Matchers.define :be_a_public_method_of do |obj|
     "be a public method"
   end
 end
+
+RSpec::Matchers.define :match_follower do |expected|
+  match do |actual|
+    expected.follower_type == actual.class.to_s && expected.follower_id == actual.id
+  end
+end
+
+RSpec::Matchers.define :match_followable do |expected|
+  match do |actual|
+    expected.followable_type == actual.class.to_s && expected.followable_id == actual.id
+  end
+end
+
+RSpec::Matchers.define :match_liker do |expected|
+  match do |actual|
+    expected.liker_type == actual.class.to_s && expected.liker_id == actual.id
+  end
+end
+
+RSpec::Matchers.define :match_likeable do |expected|
+  match do |actual|
+    expected.likeable_type == actual.class.to_s && expected.likeable_id == actual.id
+  end
+end
+
+RSpec::Matchers.define :match_mentioner do |expected|
+  match do |actual|
+    expected.mentioner_type == actual.class.to_s && expected.mentioner_id == actual.id
+  end
+end
+
+RSpec::Matchers.define :match_mentionable do |expected|
+  match do |actual|
+    expected.mentionable_type == actual.class.to_s && expected.mentionable_id == actual.id
+  end
+end
