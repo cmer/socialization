06e5e05006c907068b9e129caed2897207d0270c
Luke Rohde <lukerohde@gmail.com>
2015-10-26 23:37:22 +1100

    fix sti bug, seems to be working

--- lib/socialization/stores/active_record/follow.rb
+++ lib/socialization/stores/active_record/follow.rb
@@ -9,12 +9,12 @@ module Socialization
       belongs_to :followable, :polymorphic => true
 
       scope :followed_by, lambda { |follower| where(
-        :follower_type   => follower.class,
+        :follower_type   => follower.class.table_name.classify,
         :follower_id     => follower.id)
       }
 
       scope :following,   lambda { |followable| where(
-        :followable_type => followable.class,
+        :followable_type => followable.class.table_name.classify,
         :followable_id   => followable.id)
       }
 
@@ -54,8 +54,8 @@ module Socialization
         def followers_relation(followable, klass, opts = {})
           rel = klass.where(:id =>
             self.select(:follower_id).
-              where(:follower_type => klass).
-              where(:followable_type => followable.class.to_s).
+              where(:follower_type => klass.table_name.classify).
+              where(:followable_type => followable.class.table_name.classify).
               where(:followable_id => followable.id)
           )
 
@@ -80,8 +80,8 @@ module Socialization
         def followables_relation(follower, klass, opts = {})
           rel = klass.where(:id =>
             self.select(:followable_id).
-              where(:followable_type => klass.classify).
-              where(:follower_type => follower.class.to_s).
+              where(:followable_type => klass.table_name.classify).
+              where(:follower_type => follower.class.table_name.classify).
               where(:follower_id => follower.id)
           )
 
@@ -104,13 +104,13 @@ module Socialization
 
         # Remove all the followers for followable
         def remove_followers(followable)
-          self.where(:followable_type => followable.class.name.classify).
+          self.where(:followable_type => followable.class.table_name.classify).
                where(:followable_id => followable.id).destroy_all
         end
 
         # Remove all the followables for follower
         def remove_followables(follower)
-          self.where(:follower_type => follower.class.name.classify).
+          self.where(:follower_type => follower.class.table_name.classify).
                where(:follower_id => follower.id).destroy_all
         end
 
